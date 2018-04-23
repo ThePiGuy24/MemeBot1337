5a412d51269fbe0942bddfe74d4a9b792fcd1279
ThePiGuy24 <30549015+ThePiGuy24@users.noreply.github.com>
2018-04-23 18:15:03 +0100

    Add files via upload

---
+++ !nstaller (run first).bat
@@ -0,0 +1,5 @@
+@echo off
+py -m pip install wheel
+py -m pip install google_api_python_client-1.6.6-py2.py3-none-any.whl
+echo you can now run the bot
+pause
--- classroom bot.py
+++ classroom bot.py
@@ -1,11 +1,16 @@
 from __future__ import print_function
+from oauth2client import file, client, tools
 from apiclient.discovery import build
 from httplib2 import Http
-from oauth2client import file, client, tools
-import random
+import random, tkinter
+from tkinter import LEFT,RIGHT,TOP,BOTTOM
 
 print("imported")
-# Setup the Classroom API
+
+send = False
+
+cID = 7993359083
+
 SCOPES = 'https://www.googleapis.com/auth/classroom.coursework.students'
 store = file.Storage('credentials.json')
 creds = store.get()
@@ -14,27 +19,55 @@ if not creds or creds.invalid:
     creds = tools.run_flow(flow, store)
 service = build('classroom', 'v1', http=creds.authorize(Http()))
 print("set up")
-# Call the Classroom API
-##results = service.courses().list(pageSize=10).execute()
-##courses = results.get('courses', [])
-print("running")
-##if not courses:
-##    print('No courses found.')
-##else:
-##    print('Courses:')
-##    for course in courses:
-##        if course['name'] == "memes":
-##            print(course['name'],course['id'])
-##            cID = course['id']
-cID = 7993359083
 
-for c in range(5):
+def start():
+	global send
+	if not send:
+		send = True
+		print("Starting")
+	else:
+		print("Already Running")
+		
+def stop():
+	global send
+	if send:
+		send = False
+		print("Stopping")
+	else:
+		print("Already Stopped")
+		
+def post(title,description):
 	courseWork = {  
-		'title': 'hello from python',  
-		'description': str(random.randint(0,1000000000000000000000000000000000)),  
+		'title': title,  
+		'description': description,  
 		'workType': 'ASSIGNMENT',  
 		'state': 'PUBLISHED',  
 	}
 	courseWork = service.courses().courseWork().create(  
 		courseId=cID, body=courseWork).execute()  
 	print('Assignment created with ID {0}'.format(courseWork.get('id')))
+
+top = tkinter.Tk()
+startButton = tkinter.Button(top,text = "Start", command = start)
+stopButton = tkinter.Button(top,text = "Stop", command = stop)
+titleLabel = tkinter.Label(top,text = "Google Classroom Spam Bot Tool Thing")
+titleBoxLabel = tkinter.Label(top,text = "Title:")
+descriptionBoxLabel = tkinter.Label(top,text = "Description:")
+titleBox = tkinter.Entry(top)
+descriptionBox = tkinter.Entry(top)
+
+titleLabel.pack(side = TOP)
+titleBoxLabel.pack(side = LEFT)
+titleBox.pack(side = LEFT)
+descriptionBoxLabel.pack(side = LEFT)
+descriptionBox.pack(side = LEFT)
+stopButton.pack(side = RIGHT)
+startButton.pack(side = RIGHT)
+top.title("Google Classroom Spam Bot")
+top.iconbitmap("favicon.ico")
+
+while True:
+	if send:
+		post(titleBox.get(),descriptionBox.get())
+	top.update_idletasks()
+	top.update()
